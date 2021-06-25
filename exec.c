c0cc09080caa186e0dc9e12961feed42c748da46
Igor vinicius Neumann de paiva <ineumann@c1r6s6.42madrid.com>
2021-06-25 19:37:15 +0200

    arreglado el bug maldito... de la peor forma

--- exec.c
+++ exec.c
@@ -6,7 +6,7 @@
 /*   By: ineumann <ineumann@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/14 18:22:39 by ineumann          #+#    #+#             */
-/*   Updated: 2021/06/25 19:28:41 by ineumann         ###   ########.fr       */
+/*   Updated: 2021/06/25 19:36:21 by ineumann         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,6 @@ int	executor(t_cmd *cmd)
 	else
 		pipes(cmd);
 	wait (0);
-	ft_putstr("\r\n");
 	if (tcsetattr(STDIN_FILENO, TCSAFLUSH, &cmd->raw->raw) == -1)
 		die("tcsetattr", cmd->raw);
 	return (0);
