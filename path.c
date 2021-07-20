0aa8e6ecec34fec0a890f40477f9dedba136d8f3
NunuuuAP <nunuap@gmail.com>
2021-07-20 18:39:22 +0200

    pull

--- path.c
+++ path.c
@@ -6,7 +6,7 @@
 /*   By: narroyo- <narroyo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/22 19:10:00 by narroyo-          #+#    #+#             */
-/*   Updated: 2021/06/30 10:47:04 by narroyo-         ###   ########.fr       */
+/*   Updated: 2021/07/19 15:40:06 by narroyo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -58,44 +58,3 @@ void	ft_path(t_cmd *cmd)
 			i++;
 	}
 }
-
-/*void	ft_path(t_cmd *cmd) // OLD PATH
-{
-	int		i;
-	char	*to_find;
-	char	*tmp;
-	char	**command;
-	pid_t	pid;
-
-	i = 0;
-	pid = fork();
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &cmd->raw->orig);
-	command = ft_split(cmd->buff, ' ');
-	if (pid == -1)
-		perror("fork error");
-	while (cmd->envp->prev)
-		cmd->envp = cmd->envp->prev;
-	while (ft_strcmp(cmd->envp->key, "PATH") != 0)
-		cmd->envp = cmd->envp->next;
-	cmd->path = ft_split(cmd->envp->value, ':');
-	while (cmd->path[i])
-	{
-		tmp = ft_strjoin(cmd->path[i], "/");
-		to_find = ft_strjoin(tmp, command[0]);
-		free(tmp);
-		if (open(to_find, O_RDONLY) == -1)
-		{
-			free(to_find);
-			i++;
-		}
-		else if (pid == 0)
-		{
-			tmp = command[0];
-			command[0] = to_find;
-			free(tmp);
-			execve(to_find, command, cmd->env);
-			exit(0);
-		}
-	}
-	tcsetattr(STDIN_FILENO, TCSAFLUSH, &cmd->raw->raw);
-}*/
