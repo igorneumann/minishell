9fd9d21acf95ab7ec8e278c9c902eb6dd547ca02
NunuuuAP <nunuap@gmail.com>
2021-07-28 17:20:09 +0200

    PETA, NO TE LO DESCARGUES

--- quotes.c
+++ quotes.c
@@ -6,7 +6,7 @@
 /*   By: narroyo- <narroyo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/07/22 19:05:47 by narroyo-          #+#    #+#             */
-/*   Updated: 2021/07/27 19:06:00 by narroyo-         ###   ########.fr       */
+/*   Updated: 2021/07/28 17:19:33 by narroyo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 
 char	*search_value(char *elem, t_cmd *cmd)
 {
+	elem++;
 	while (ft_strcmp(elem, cmd->envp->key) != 0)
 		cmd->envp = cmd->envp->next;
 	if (cmd->envp->value != NULL && ft_strcmp(elem, cmd->envp->key) == 0)
@@ -53,14 +54,18 @@ void	replace(t_cmd *cmd, int position, int old_len)
 	int	counter;
 
 	counter = 0;
+	position--;
 	new_len = ft_strlen(cmd->dollar_value[position]);
+	printf("%s\n", cmd->tmp_in);
+	printf("%lu\n", ft_strlen(cmd->tmp_in) + new_len - old_len + 1);
 	diff = ft_strlen(cmd->tmp_in) + new_len - (old_len + 1);
 	cmd->in = (char *)malloc(sizeof(char) * diff + 1);
+	if (cmd->in == NULL)
+		return ;
 	while (cmd->tmp_in != '\0')
 	{
 		if (*cmd->tmp_in == '$')
 		{
-			counter++;
 			if (counter == position)
 			{
 				while (*cmd->dollar_value[position])
@@ -69,6 +74,8 @@ void	replace(t_cmd *cmd, int position, int old_len)
 					cmd->dollar_value[position]++;
 					cmd->in++;
 				}
+				cmd->tmp_in++;
+				counter++;
 			}
 		}
 		else
@@ -89,7 +96,6 @@ int	dollar(t_cmd *cmd, int k)
 
 	i = 0;
 	ch = 0;
-	cmd->dollar_value = (char **)malloc(sizeof(char *) * 5);
 	if (cmd->quote_s % 2 != 0)
 		return (0);
 	if (cmd->quote_s % 2 == 0)
@@ -101,8 +107,7 @@ int	dollar(t_cmd *cmd, int k)
 			ch++;
 			i++;
 		}
-		var = ft_strdup(cmd->tmp_in + i);
-		var[ch] = '\0';
+		var = ft_strdup(cmd->tmp_in);
 		cmd->dollar_value[k] = ft_strdup(search_value(var, cmd));
 		free(var);
 		k++;
@@ -129,30 +134,50 @@ int	quotes(t_cmd *cmd)
 	return (i);
 }
 
+int	count_char(char *line, char character)
+{
+	int	i;
+
+	i = 0;
+	while (*line)
+	{
+		if (*line == character)
+			i++;
+		line++;
+	}
+	return (i);
+}
+
 void	check_replacement(t_cmd *cmd)
 {
 	int	k;
+	int	i;
 
 	k = 0;
-	cmd->tmp_in = ft_strdup(cmd->in);
-	free(cmd->in);
-	while (*cmd->tmp_in)
+	i = 0;
+	if (ft_strchr(cmd->in, '$') != NULL)
 	{
-		if (quotes(cmd) != 0 && *cmd->tmp_in == '$')
+		cmd->tmp_in = ft_strdup(cmd->in);
+		cmd->dollar_value = (char **)malloc(sizeof(char *)
+			* count_char(cmd->tmp_in, '$'));
+		free(cmd->in);
+	}
+	while (cmd->tmp_in[i])
+	{
+		if (quotes(cmd) != 0 && cmd->tmp_in[i] == '$')
 		{
 			if (look_for_closure('\"', '$', cmd->tmp_in) == 1)
 			{
 				if (look_for_closure('\'', '$', cmd->tmp_in) == 1)
-					cmd->tmp_in++;
+					i++;
 				else
 					k = dollar(cmd, k);
 			}
 		}
-		else if (quotes(cmd) == 0 && *cmd->tmp_in == '$')
+		else if (quotes(cmd) == 0 && cmd->tmp_in[i] == '$')
 			k = dollar(cmd, k);
-		cmd->tmp_in++;
+		i++;
 	}
-	printf("%s\r\n", cmd->tmp_in);
-	if (k == 0)
+	if (ft_strchr(cmd->in, '$') != NULL)
 		cmd->in = ft_strdup(cmd->tmp_in);
 }
