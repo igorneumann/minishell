2a92ec1af2c17f623425f890cba7142a6ab5465f
NunuuuAP <nunuap@gmail.com>
2021-08-19 21:14:09 +0200

    Funcionan las comillas, pero no salen con el dollar

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: narroyo- <narroyo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/02/15 20:20:24 by narroyo-          #+#    #+#             */
-/*   Updated: 2021/08/18 13:59:07 by narroyo-         ###   ########.fr       */
+/*   Updated: 2021/08/19 21:07:30 by narroyo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -207,6 +207,8 @@ int		ft_altarrow(t_cmd *cmd);
 int		look_for_closure(char quote, char dollar, char *line, int i);
 int		count_char(char *line, char character);
 int		check_quotes_error(t_cmd *cmd);
+int		look_for_open(char quote, char *str, int i);
+char	*replace_quotes(char *with_quotes);
 int		check_replacement(t_cmd *cmd);
 
 /*
