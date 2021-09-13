a34bd8c4491f2cc4e9035b74b24b51af9f60ef99
NunuuuAP <nunuap@gmail.com>
2021-09-13 14:00:45 +0200

    Falta la norma del echo

--- echo.c
+++ echo.c
@@ -6,7 +6,7 @@
 /*   By: narroyo- <narroyo-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/09 21:20:06 by narroyo-          #+#    #+#             */
-/*   Updated: 2021/09/09 21:24:03 by narroyo-         ###   ########.fr       */
+/*   Updated: 2021/09/13 13:57:06 by narroyo-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,27 +14,42 @@
 
 int	print_echo_nl(t_cmd *cmd, int i, int j)
 {
-	if (ft_strnstr(cmd->in + i, "-n ", 3) != NULL)
+	if (ft_strnstr(cmd->in + i, "-n", 2) != NULL)
 	{
 		cmd->echo_nl = 1;
+		if (cmd->in[i + j] == '-' && cmd->in[i + j + 1] == 'n')
+		{
+			j += 2;
+			while (cmd->in[i + j] == 'n')
+				j++;
+			if (!cmd->in[i + j])
+				return (j + 3);
+		}
 		while (cmd->in[i + j])
 		{
 			if (cmd->in[i + j] == '-')
 				j++;
 			while (cmd->in[i + j] == 'n')
 				j++;
+			if (cmd->in[i + j] != ' ')
+				cmd->echo_nl = 2;
 			while (cmd->in[i + j] == ' ' && cmd->in[i + j + 1] == '-'
 				&& cmd->in[i + j + 2] == 'n')
 				j += 3;
-			if (cmd->in[i + j] != ' ')
-				cmd->echo_nl = 2;
+			while (cmd->in[i + j] == 'n')
+				j++;
 			while (cmd->in[i + j] == ' ')
 				j++;
 			if (ft_strnstr(cmd->in + i + j, "-n", 2) == NULL)
 				break ;
 		}
 		if (cmd->echo_nl == 1)
+		{
+			if (cmd->in[i + j - 1] != ' ' && cmd->in[i + j] != '\0')
+				while (cmd->in[i + j] != '-')
+					j--;
 			ft_putstr(&cmd->in[i + j]);
+		}
 	}
 	return (j);
 }
@@ -42,11 +57,16 @@ int	print_echo_nl(t_cmd *cmd, int i, int j)
 void	print_echo(t_cmd *cmd, int i, int j)
 {
 	j = print_echo_nl(cmd, i, j);
-	if (cmd->echo_nl == 0 || cmd->echo_nl == 2)
+	if (cmd->echo_nl != 1)
 	{
 		if (cmd->echo_nl == 2)
-			j -= 3;
+		{
+			while (cmd->in[i + j] != '-')
+				j--;
+		}
 		ft_putstr(&cmd->in[i + j]);
+		if (cmd->echo_nl == 2)
+			j = 0;
 		if (j == 0)
 			ft_putstr("\r\n");
 	}
